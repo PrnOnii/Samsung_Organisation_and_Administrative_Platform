238afacaaefc127dfd3a0e1927d99e871c7e6290
Kevan Sadeghi <ksadeghi27@gmail.com>
2018-06-08 09:16:42 +0200

    Auto stash before merge of "master" and "origin/master"

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -21,9 +21,71 @@
                         <a class="dropdown-item action" id="justify" href="#">Ajouter une excuse</a>
                     </div>
                 </div>
-                <div class="table-responsive">
-                    <table class="table table-sm table-striped dataTable">
-                        <thead>
+                <table class="table table-sm table-striped dataTable">
+                    <thead>
+                    <tr>
+                        <th><i class="far fa-check-square"></i></th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Pangs</th>
+                        <th>Promotion</th>
+                        <th>Check-in</th>
+                        <th>Check-out</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($students as $student)
+                            <tr>
+                            <td data-toggle="tooltip" title="<img src='https://cdn.local.epitech.eu/userprofil/profilview/{{ $student->first_name }}.{{ $student->last_name }}.jpg' />" class="clickable"><input name="students[]" value="{{ $student->id }}" type="checkbox"></td>
+                            <td data-toggle="tooltip" title="<img src='https://cdn.local.epitech.eu/userprofil/profilview/{{ $student->first_name }}.{{ $student->last_name }}.jpg' />" class="clickable">
+                                <a href="/student/{{ $student->first_name }}.{{ $student->last_name }}"> {{ ucfirst($student->first_name) }}</a>
+                            </td>
+                            <td data-toggle="tooltip" title="<img src='https://cdn.local.epitech.eu/userprofil/profilview/{{ $student->first_name }}.{{ $student->last_name }}.jpg' />" class="clickable">
+                                <a href="/student/{{ $student->first_name }}.{{ $student->last_name }}"> {{ ucfirst($student->last_name) }}</a>
+                            </td>
+                            <td data-toggle="tooltip" title="<img src='https://cdn.local.epitech.eu/userprofil/profilview/{{ $student->first_name }}.{{ $student->last_name }}.jpg' />" class="clickable">
+                                <h4>
+                                @if ($student->pangs <= 0)
+                                    <span class="badge badge-danger">
+                                @elseif ($student->pangs <= 300)
+                                    <span class="badge badge-warning">
+                                @elseif ($student->pangs >= 700)
+                                    <span class="badge badge-success">
+                                @else
+                                    <span class="badge">
+                                @endif
+                                {{ $student->pangs }}
+                                </span></h4>
+                            </td>
+                            <td data-toggle="tooltip" title="<img src='https://cdn.local.epitech.eu/userprofil/profilview/{{ $student->first_name }}.{{ $student->last_name }}.jpg' />" class="clickable">{{ $student->promo->name }}</td>
+                            <td>
+                            @if (is_object($student->checkIn) && $student->checkIn->day === \Carbon\Carbon::now()->toDateString() && $student->checkIn->arrived_at !== null)
+                                {{ $student->checkIn->arrived_at }}
+                            @else
+                                <form method="post" class="checkIn" action="{{ route("checkIn") }}">
+                                    {!! csrf_field() !!}
+                                    <input type="hidden" name="id" value="{{ $student->id }}">
+                                    <button type="submit" class="btn btn-success btn-sm">Check-In</button>
+                                </form>
+                            @endif
+                            </td>
+                            <td>
+                            @if (is_object($student->checkIn) && $student->checkIn->day === \Carbon\Carbon::now()->toDateString() && $student->checkIn->leaved_at !== null)
+                                {{ $student->checkIn->leaved_at }}
+                            @else
+                                @if (is_object($student->checkIn) && $student->checkIn->day === \Carbon\Carbon::now()->toDateString() && $student->checkIn->arrived_at !== null)
+                                <form method="post" class="checkOut" action="{{ route("checkOut") }}">
+                                    {!! csrf_field() !!}
+                                    <input type="hidden" name="id" value="{{ $student->id }}">
+                                    <button type="submit" class="btn btn-warning btn-sm">Check-Out</button>
+                                </form>
+                                @endif
+                            @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                    <tfoot>
                         <tr>
                             <th><i class="far fa-check-square"></i></th>
                             <th>First Name</th>
