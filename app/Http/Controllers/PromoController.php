99555b80c46c1f3e160a47840a7a5513c4bf2e4e
PrnOnii <ksadeghi27@gmail.com>
2018-07-01 19:41:21 +0200

    Update PromoController
    corrected middleware error

--- app/Http/Controllers/PromoController.php
+++ app/Http/Controllers/PromoController.php
@@ -9,7 +9,7 @@ class PromoController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', 'admin');
+        $this->middleware('admin');
     }
     /**
      * Display a listing of the resource.
