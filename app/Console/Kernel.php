d24e7647c70884f1924f83455e7c6a349002df4e
Rikette <baptiste.chiocca@gmail.com>
2018-07-03 14:59:27 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new ProcessPangs)->everyMinute();
+        $schedule->job(new ProcessPangs)->daily();
     }
 
     /**
